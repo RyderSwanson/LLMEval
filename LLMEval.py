f7895eba75f5319338c19957fb6e0c37db164523
ryderswanson <ryderswanson@gmail.com>
2024-10-03 17:56:42 -0700

    Fix bug (Models no longer takes api key) More interesting response when used from command line

--- LLMEval.py
+++ LLMEval.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     import tests.args as args
     from Models import Models
     llm = LLMEval(api_key=args.api_key)
-    models = Models(api_key=args.api_key)
+    models = Models()
     model = models.getRandomFreeModelID()
     response = llm.getResponse(prompt="What is the meaning of life?", model=model)
     print(response)
@@ -45,10 +45,13 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Tool to measure the performance of LLMs')
     parser.add_argument('--api_key', type=str, help='API Key', required=True)
-    parser.add_argument('--model', type=str, default="openai/gpt-3.5-turbo", help='Model')
+    parser.add_argument('--model', type=str, default=models.getRandomFreeModelID(), help='Model')
     parser.add_argument('--prompt', type=str, default="What is the meaning of life?", help='Prompt')
     args = parser.parse_args()
 
     llm = LLMEval(api_key=args.api_key)
-    response = llm.getResponse(prompt=args.prompt)
-    print(response)
+    response = llm.getResponse(prompt=args.prompt, model=args.model)
+
+    print(f"Model used: {args.model}\n")
+    print(f"Prompt: {args.prompt}\n")
+    print(f"Model response: {response['choices'][0]['message']['content']}")
