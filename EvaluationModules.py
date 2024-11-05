0b02e7a5cfc42af515a541fd21c58d335f5afcdc
Elliott-Hendrickson <100046451+Elliott-Hendrickson@users.noreply.github.com>
2024-11-05 15:08:37 -0800

    Additional Evaluator Functionality
    Added evaluators, and the evaluator handler, they are both work in progress. Some work, some don't, need more.

--- EvaluationModules.py
+++ EvaluationModules.py
@@ -1,7 +1,15 @@
 from abc import ABC, abstractmethod
-import evaluate
 from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.meteor_score import meteor_score
 import torch
+import evaluatorDependencies.chrF as characterLevelScore
+import jiwer
+from transformers import BertTokenizer, BertModel
+from bert_score import BERTScorer
+from nltk.corpus import stopwords
+from nltk import download
+from gensim.models import Word2Vec, KeyedVectors
+# import evaluatorDependencies.factscorer as FS
 
 """
     The Abstract EvaluationHandler Class which takes the prompt, response, and returns the evaluation metric class
@@ -23,11 +31,31 @@ class EvaluationMethodFactory:
         else:
             raise ValueError("Unsupported data type for evaluation")
 
+def printHelloWorld():
+
+    print("HelloWorld")
+
 class Evaluator(ABC):
     @abstractmethod
     def PerformEvaluation(self):
         pass
 
+def getEvaluators():
+    return [
+        "perp",
+        "bleu",
+        "rouge",
+        "meteor",
+        "chrf",
+        "wer",
+        "editDistance",
+        "bert",
+        "wordMover",
+        "ttr",
+        "mtld",
+        "hdd"
+        # "factScorer"
+    ]
 
 """
     Data will be in the formate: [List Of Strings, List of strings with proper grammer]
@@ -122,3 +150,186 @@ class rougeEvaluator(Evaluator):
         rouge_score = evaluate.load("rouge")
         return rouge_score.compute(predictions=self.data[1], references=self.data[0], use_stemmer=True)
 
+"""
+    Data will be in the format: [Base truth string, LLM Output String]
+"""
+class meteorEvaluator(Evaluator):
+
+    def __init__(self, data):
+
+        self.data = data
+    
+    def PerformEvaluation(self):
+
+        return meteor_score(list([self.data[1]]), list([self.data[2]]))
+
+"""
+    Data will be in the format: [Base truth string, LLM Output string]
+"""
+class chrFEvaluator(Evaluator):
+
+    def __init__(self, data):
+
+        self.data = data
+    
+    def PerformEvaluation(self):
+
+        return characterLevelScore.chrF(self.data[1], self.data[2])
+
+"""
+    Data will be in the format: [Base truth string, LLM Output String]
+"""
+class werEvalutor(Evaluator):
+
+    def __init__(self, data):
+
+        self.data = data
+    
+    def PerformEvaluation(self):
+
+        transforms = jiwer.Compose(
+            [
+                jiwer.ExpandCommonEnglishContractions(),
+                jiwer.RemoveEmptyStrings(),
+                jiwer.ToLowerCase(),
+                jiwer.RemoveMultipleSpaces(),
+                jiwer.Strip(),
+                jiwer.RemovePunctuation(),
+                jiwer.ReduceToListOfListOfWords(),
+            ])
+        
+        return jiwer.wer(self.data[0], self.data[1], truth_transform=transforms, hypothesis_transform=transforms)
+
+"""
+    Data will be in the following format [Truth String, LLM Output String]
+"""
+class editDistanceEvalutor(Evaluator):
+
+    def __init__(self, data):
+
+        self.data = data
+    
+    def PerformEvaluation(self):
+
+        s1 = self.data[0]
+        s2 = self.data[1]
+
+        m, n = len(s1), len(s2)
+
+        # Create a table to store results of subproblems
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # Fill the known entries in dp[][]
+        # If one string is empty, then answer 
+        # is length of the other string
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+
+        # Fill the rest of dp[][]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+
+        return dp[m][n]
+    
+"""
+    Data will be in the format: [Base truth string, LLM Output string]
+    Output: A dictionary with each value and its corresponding evaluation metric
+"""
+class bertEvaluator(Evaluator):
+
+    def __init__(self, data):
+
+        self.data = data
+    
+    def PerformEvaluation(self):
+
+        P, R, F1 = BERTScorer(model_type='bert-base-uncased').score([self.data[1]], [self.data[0]])
+        return {"Precision": P.mean(), "Recall": R.mean(), "F1":F1.mean()}
+    
+"""
+    Data will be in the format: [Base truth string, LLM Output string]
+"""
+class wordMoverEvaluator(Evaluator):
+
+    def __init__(self, data):
+
+        self.data = [data[0].lower.split(),data[1].lower.split()]
+    
+    def PerformEvaluation(self):
+
+        download("stopwords")
+        stop_words = stopwords.words("english")
+        cleanedBaseSentence = [w for w in self.data[0] if w not in stop_words]
+        cleanedHypothesisSentence = [w for w in self.data[1] if w not in stop_words]
+        
+        model = KeyedVectors.load_word2vec_format('cc.en.100.bin', binary=True)
+
+        return model.wmdistance(cleanedBaseSentence, cleanedHypothesisSentence)
+    
+# """
+#     The fact scorer method is unique as it is brand new and uses a model trained on wikipedia for accuracy requires this model to be self contained
+#     Data will be in the formate [topic, LLM output string, llmAPIKey]
+# """
+# class factScorerEvaluator(Evaluator):
+
+#     def __init__(self, data):
+
+#         self.data = list([data[0]]), list([data[1]], data[2])
+    
+#     def PerformEvaluation(self):
+
+#         fs = FS.FactScorer(openai_key = self.data[2])
+#         return fs.get_score(self.data[0], self.data[1], gamma=1)["score"]
+"""
+    Data will be in the format: [List Of Strings]
+"""
+class ttrEvaluator(Evaluator):
+
+    def init(self, data):
+
+        self.data = data
+
+    def PerformEvaluation(self):
+        ld_object = ld.LexicalDiversity(self.data)
+
+        ttr = ld_object.ttr
+
+        return ttr
+
+"""
+    Data will be in the format: [List Of Strings]
+"""
+class mtldEvaluator(Evaluator):
+
+    def init(self, data):
+
+        self.data = data
+
+    def PerformEvaluation(self):
+        ld_object = ld.LexicalDiversity(self.data)
+
+        mtld = ld_object.mtld()
+
+        return mtld
+
+"""
+    Data will be in the format: [List Of Strings]
+"""
+class hddEvaluator(Evaluator):
+
+    def init(self, data):
+
+        self.data = data
+
+    def PerformEvaluation(self):
+        ld_object = ld.LexicalDiversity(self.data)
+
+        hdd = ld_object.hdd()
+
+        return hdd
